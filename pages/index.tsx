648999782bcc99f075fdac184086daab2f6fdb33
AhmedHassan97 <ahahmed202025@gmail.com>
2021-12-27 16:29:11 +0200

    feat: add login page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import LoginComponent from "../app/components/login/login";
+const Home: NextPage = () => {
+  return (
+    <div>
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default Home;
