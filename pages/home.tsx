7846484a494d9da5a4cd96fca4f7aacf587c3833
AhmedHassan97 <ahahmed202025@gmail.com>
2021-12-27 16:28:59 +0200

    feat: add home page

---
+++ pages/home.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+import HomeComponent from "../app/components/home/HomeComponent";
+const home: NextPage = () => {
+  return <HomeComponent />;
+};
+
+export default home;
