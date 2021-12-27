0dc478ddc5024719c35c9eadbaa0da23c71ecf6c
AhmedHassan97 <ahahmed202025@gmail.com>
2021-12-27 15:55:04 +0200

    feat: add post card component

---
+++ app/components/posts/PosteCardComponent.tsx
@@ -0,0 +1,23 @@
+import { useRouter } from "next/router";
+const PostCardComponent = (props: any) => {
+  const router = useRouter();
+  return (
+    <div>
+      <div className="card lg:card-side card-bordered">
+        <div className="card-body">
+          <h2 className="card-title">{props.title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: props.content }}></div>
+          <div className="card-actions">
+            <button
+              className="btn btn-primary"
+              onClick={() => router.push(`/${props.id}`)}
+            >
+              More info
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PostCardComponent;
